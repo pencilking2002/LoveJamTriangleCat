c70c93b35ade61f3ba251d0cf57a3ee9faa82a8e
Roman <pencilking2002@gmail.com>
2016-06-11 18:10:39 -0400

    Player animator controller controller code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,14 +11,14 @@ public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed = 10.0f;
 	public Image IconContainer;
-	private bool canMove = true;
 
 	//---------------------------------------------------------------------------------------------------------------------------
 	// Private Variables
 	//---------------------------------------------------------------------------------------------------------------------------
-
+	private bool canMove = true;
 	private Vector2 moveDirection = Vector3.zero;
 	private Rigidbody2D rb;
+	private Animator animator;
 
 	private Dictionary <string, Image> iconsDict = new Dictionary<string, Image>();
 	private Dictionary <string, Sprite> hiddenIconSprites = new Dictionary<string, Sprite>();
@@ -28,11 +28,13 @@ public class PlayerController : MonoBehaviour {
 	public Sprite hiddenIcon3;
 	public Sprite hiddenIcon4;
 
+	private int anim_moveDirection = Animator.StringToHash("moveDirection");
 
 	// Use this for initialization
 	void Awake () 
 	{
 		rb = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
 		GameManager.CacheIcons(iconsDict, IconContainer);
 		CacheHiddenIconSprites();
 		//ObfuscateIcons();
@@ -58,18 +60,42 @@ public class PlayerController : MonoBehaviour {
 		moveDirection = new Vector2(InputController.h, InputController.v);
 		moveDirection = Vector2.ClampMagnitude(moveDirection, 1.0f);
 		rb.velocity = moveDirection * moveSpeed;
+
+		// Going right
+		if (InputController.h > 0)
+		{
+			animator.SetInteger(anim_moveDirection, 3);
+		}
+		// Going left
+		else if (InputController.h < 0)
+		{
+			animator.SetInteger(anim_moveDirection, 1);
+		}
+		// Going up or down
+		else if (Mathf.Abs(InputController.v) > 0)
+		{
+			animator.SetInteger(anim_moveDirection, 2);
+		} 
+		// else Idle
+		else 
+		{
+			animator.SetInteger(anim_moveDirection, 0);
+		}
 	}
+
 
 	void OnCollisionEnter2D(Collision2D col)
 	{
 		if (col.gameObject.CompareTag("npc"))
 		{
 			print ("collision");
+			canMove = false;
 			col.gameObject.GetComponent<NPCController>().EnableIconContainer();
 		}
 
 	}
 
+	// Change 
 	void RevealIcon(Image icon)
 	{
 		string iconTag = icon.tag;

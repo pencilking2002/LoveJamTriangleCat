52a5404c3d1fb42680dfc9a4333e36233009a605
Roman <pencilking2002@gmail.com>
2016-06-11 10:35:23 -0400

    Installed InControl and LeanTween

---
+++ Assets/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	//public PlayerController player;
+	public static GameManager Instance;
+	public static float v, h;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		if(Instance == null)
+			Instance = this;
+		else 
+			Destroy(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/InputController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+
+	public static InputController Instance;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		if(Instance == null)
+			Instance = this;
+		else 
+			Destroy(this);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}

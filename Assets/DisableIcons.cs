a63ad809d72026a34cbff39235b7ce4f5d746b00
Roman <pencilking2002@gmail.com>
2016-06-12 08:13:46 -0400

    blah

---
+++ Assets/DisableIcons.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableIcons : MonoBehaviour {
+
+	public bool disable = true;
+	public GameObject[] icons = new GameObject[4];
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (disable)
+		{
+			foreach(GameObject i in icons)
+				i.SetActive(false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

cf98e23175894bea16aa65f871f1b3ef64b3e83d
Dan Lance <danvlance@gmail.com>
2016-06-12 11:49:34 -0400

    Reworked environment elements into prefabs. Copy then from MainSceneDan3 if you want it all to look right again.

---
+++ Assets/environmentPlacement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class environmentPlacement : MonoBehaviour {
+
+    [SerializeField]
+    private SpriteRenderer render;
+
+    [SerializeField]
+    private Sprite[] possibleSprites;
+
+	void Start () {
+        render.sprite = possibleSprites[Random.Range(0, possibleSprites.Length)];
+        render.sortingOrder = (Mathf.RoundToInt(transform.position.y * -100));
+    }
+	
+	void Update () {
+	
+	}
+}
